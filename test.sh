1bb77b6b01035173a518a675777e9a67b3952e1e
Dima Zakharov <zakharov@ibm.com>
2026-01-15 14:26:33 +0000

    simple bash test

---
+++ test.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env -S bash
+
+
+
+# update exe to required executable
+EXE="math-tool-stdio -Dquarkus.log.console.enable=false -Dquarkus.banner.enabled=false "
+
+
+#commands
+
+INIT='{"jsonrpc":"2.0","id":1,"method":"initialize","params":{"protocolVersion":"2024-11-05","capabilities":{},"clientInfo":{"name":"test-client","version":"1.0"}}}'
+NOTIFY='{"jsonrpc":"2.0","method":"notifications/initialized"}'
+LIST='{"jsonrpc":"2.0","id":2,"method":"tools/list"}'
+
+CALL='{
+  "jsonrpc": "2.0",  
+  "id": 3,  
+  "method": "tools/call",  
+  "params": {    
+    "name": "getSum",    
+    "arguments": {
+          "a": 15,      
+          "b": 27   
+   }}
+}'
+
+# echo $CALL | yq -o json -M -I 0
+
+(
+  echo "$INIT"
+  sleep 0.1
+  echo "$NOTIFY"
+  sleep 0.1
+  echo "$LIST"
+  sleep 0.1
+  echo "$(echo $CALL | yq -o json -M -I 0 )"
+) | $EXE
